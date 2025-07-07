f70319b0fdb3756b628d3073f3320da0520a09ec
stein-00 <30701bphs25@sonline20.sen.go.kr>
2025-07-07 13:46:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
-st.title("나의 첫 Streamlit 앱")
-
-st.header("Streamlit에 오신 것을 환영합니다!")
-
-st.write("이것은 간단한 텍스트입니다.")
-
-st.success("성공적으로 앱이 실행되었습니다! 🎉")
-import streamlit as st
-
 import numpy as np
 
 from astropy.io import fits
